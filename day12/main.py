3bb55df44cac6a8d754421093349c0f3cd6fc0d4
Jonas <jonas.kauker2003@gmail.com>
2024-12-12 17:31:12 +0100

    skill issue

--- day12/main.py
+++ day12/main.py
@@ -7,38 +7,32 @@ from typing import List
 #	area => amount of letters next to each other
 #	perimeter => sides of the edges of the field
 
-file_content: List[str] = []
+file_content: list[str] = []
 
-# replace the letter when found with a space
 def get_garden_price(x: int, y: int, letter: str) -> int:
+	perimeter = 0
+	area = 0
+
 	if file_content[x][y] != letter:
 		return -1
 
-	perimeter: int = 0
-	area: int = 0
-
 	def flood_search(x: int, y: int, letter: str) -> None:
 		nonlocal perimeter, area
-		if x < 0 or x >= len(file_content):
+		if x < 0 or y < 0 or x >= len(file_content) or y >= len(file_content[0]):
+			print(f"Incrementing perimeter at {x}, {y}")
+			print(file_content)
+			perimeter += 1
 			return
-		if y < 0 or y >= len(file_content[x]):
+		if file_content[x][y] == ' ':
+			print(f"Incrementing perimeter at {x}, {y}")
+			print(file_content)
+			perimeter += 1
 			return
 		if file_content[x][y] != letter:
 			return
 
-		row = list(file_content[x])
-		row[y] = ' '
-		file_content[x] = ''.join(row)
 		area += 1
-
-		if x + 1 >= len(file_content) or file_content[x + 1][y] != letter:
-			perimeter += 1
-		if x - 1 < 0 or file_content[x - 1][y] != letter:
-			perimeter += 1
-		if y + 1 >= len(file_content[x]) or file_content[x][y + 1] != letter:
-			perimeter += 1
-		if y - 1 < 0 or file_content[x][y - 1] != letter:
-			perimeter += 1
+		file_content[x] = file_content[x][:y] + ' ' + file_content[x][y+1:]
 
 		flood_search(x + 1, y, letter)
 		flood_search(x - 1, y, letter)
@@ -46,6 +40,8 @@ def get_garden_price(x: int, y: int, letter: str) -> int:
 		flood_search(x, y - 1, letter)
 
 	flood_search(x, y, letter)
+
+	print(f"Letter: {letter} Area: {area}, Perimeter: {perimeter}")
 
 	return perimeter * area
 
@@ -57,12 +53,10 @@ def get_total_cost() -> int:
 				cost += get_garden_price(x, y, file_content[x][y])
 	return cost
 
-
 def get_file_content(path: str) -> None:
 	with open(path, 'r') as file:
 		for line in file:
 			file_content.append(line.strip())
-
 
 def main():
 	if len(argv) != 2:
@@ -73,6 +67,15 @@ def main():
 		print("Failed to read or get file contents")
 		return
 	print(f"Total cost: {str(get_total_cost())}")
+
+	left = 0
+	for line in file_content:
+		for letter in line:
+			if (letter != ' '):
+				print(letter)
+				left += 1
+	print(f"There are {left} letters left")
+	print(f"Second run cost: {str(get_total_cost())}")
 
 if __name__ == "__main__":
 	main()
